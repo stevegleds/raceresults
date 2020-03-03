a7627e17af9ac9514ab1355933f653ba40dd8d3c
stevegleds <stevegleds@gmail.com>
2020-03-03 21:29:55 +0000

    Created graph of race count Created duo plot of same graph Next step is to plot minimum time

--- graphs.py
+++ graphs.py
@@ -15,15 +15,15 @@ def currency(x, pos):
 
 df = pd.read_csv(results_file)
 print(df.head())
-top_10 = (df.groupby('name')['ext price', 'quantity'].agg({
-    'ext price': 'sum',
-    'quantity': 'count'
-}).sort_values(by='ext price', ascending=False))[:10].reset_index()
+top_10 = (df.groupby('Name')['Course', 'Time'].agg({
+    'Course': 'count',
+    'Time': 'min'
+}).sort_values(by='Time', ascending=True))[:10].reset_index()
 
 top_10.rename(columns={
-    'name': 'Name',
-    'ext price': 'Sales',
-    'quantity': 'Purchases'
+    'Name': 'Name',
+    'Course': 'Runs',
+    'Time': 'Time'
 },
               inplace=True)
 
@@ -33,20 +33,20 @@ plt.style.use('ggplot')
 # Single Plot
 fig, ax = plt.subplots(figsize=(14, 6))
 # Subplot allows any future customization will be done via the ax or fig objects.
-top_10.plot(kind='barh', y='Sales', x='Name', ax=ax)
-ax.set_xlim([40000, 140000])
-ax.set(title='2014 Revenue', xlabel='Total Revenue', ylabel='Customer')
-formatter = FuncFormatter(currency)
-ax.xaxis.set_major_formatter(formatter)
+top_10.plot(kind='barh', y='Runs', x='Name', ax=ax)
+# ax.set_xlim([40000, 140000])
+ax.set(title='Number of parkruns', xlabel='Name', ylabel='Runner')
+# formatter = FuncFormatter(currency)
+# ax.xaxis.set_major_formatter(formatter)
 ax.legend().set_visible(False)
 
-# Add average line
-avg = top_10['Sales'].mean()
-ax.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=2)
-# Annotate new customers - random 3, 5, 8
-for cust in [3, 5, 8]:
-    ax.text(avg, cust, "New Customer")
-print('Showing first plot')
+# # Add average line
+# avg = top_10['Sales'].mean()
+# ax.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=2)
+# # Annotate new customers - random 3, 5, 8
+# for cust in [3, 5, 8]:
+#     ax.text(avg, cust, "New Customer")
+# print('Showing first plot')
 # This version saves the plot as a png with opaque background.
 # I have also specified the dpi and bbox_inches="tight" in order to minimize excess white space.
 fig.savefig('sales-single.png', transparent=False, dpi=80, bbox_inches="tight")
@@ -56,17 +56,17 @@ print('Creating dual plot')
 # Get the figure and axis
 fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(7, 4))
 
-# Revenue plot
-top_10.plot(kind='barh', y='Sales', x='Name', ax=ax0)
-ax0.set_xlim([-10000, 140000])
+# Count plot
+top_10.plot(kind='barh', y='Runs', x='Name', ax=ax0)
+# ax0.set_xlim([-10000, 140000])
 ax0.set(title='Revenue', xlabel='Total Revenue', ylabel='Customers')
-ax0.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=2)
+# ax0.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=2)
 
 # Unit sales plot
-top_10.plot(kind='barh', y='Purchases', x='Name', ax=ax1)
-avg = top_10['Purchases'].mean()
+top_10.plot(kind='barh', y='Runs', x='Name', ax=ax1)
+# avg = top_10['Purchases'].mean()
 ax1.set(title='Units', xlabel='Total Units', ylabel='')
-ax1.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=1)
+# ax1.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=1)
 
 # Title the figure
 fig.suptitle('2014 Sales Analysis', fontsize=14, fontweight='bold')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-    
+e line
+# avg = top_10['Sales'].mean()
+# ax.axvline(x=avg, color='b', label='Average', linestyle='--', linewidth=2)
+# # Annotate new customers - random 3, 5, 8
+# for cust in [3, 5, 8]:
+#     ax.text(avg, cust, "New Customer")
+# print('Sho
